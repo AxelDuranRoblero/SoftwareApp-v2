beead27f135a7a0bfc31dfcde55a97ee5a4c3c45
perjaaaan <benjaminjavier46@gmail.com>
2025-10-26 22:47:29 -0300

    Add gunicorn to requirements.txt

--- SoftwareApp/settings.py
+++ SoftwareApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ItemApp',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,15 @@ WSGI_APPLICATION = 'SoftwareApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app',
+        'USER': 'root',
+        'PASSWORD': '2006',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'ssl': {'ssl-mode': 'REQUIRED'},
+        },
     }
 }
 
