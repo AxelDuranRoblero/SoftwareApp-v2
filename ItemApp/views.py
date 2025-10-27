2adc3e459ccf090e16ba5475f85ee2df56ad45fc
Axel Duran <axel.duran@inacapmail.cl>
2025-10-27 02:44:40 -0300

    Implementando login NUAM y reestructurando plantillas

--- ItemApp/views.py
+++ ItemApp/views.py
@@ -1,6 +1,12 @@
-
 from django.shortcuts import render
 
-
+# Tu vista de inicio (esta ya la tienes)
 def vista_inicio(request):
     return render(request, 'inicio.html')
+
+# 
+# ¡AÑADE ESTA NUEVA VISTA!
+# 
+def vista_login_personalizado(request):
+    # Ahora sí apunta al archivo 'login.html' (el de NUAM)
+    return render(request, 'login.html')
--- SoftwareApp/urls.py
+++ SoftwareApp/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from ItemApp import views as item_views
-
-from django.contrib.auth import views as otras_views
+from ItemApp import views as item_views # 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', item_views.vista_inicio, name='inicio'),
-    path('login/', otras_views.LoginView.as_view(
-        template_name='login.html' 
-    ), name='login'),
 
-    path('logout/', otras_views.LogoutView.as_view(), name='logout'),
+    path('login/', item_views.vista_login_personalizado, name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
