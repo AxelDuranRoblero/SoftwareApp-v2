4acc0e786f097cb5164b4e1bf952b28af72c7f6f
Axel Duran <axel.duran@inacapmail.cl>
2025-10-27 03:11:39 -0300

    Añadiendo STATICFILES_DIRS para la carpeta static raíz

--- SoftwareApp/settings.py
+++ SoftwareApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +86,6 @@ WSGI_APPLICATION = 'SoftwareApp.wsgi.application'
 
 
 
-import os
 import dj_database_url
 
 DATABASES = {
