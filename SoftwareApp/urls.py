6d4afe6a28cb5522b304a485e89dc2e7601ffcb8
Axel Duran <axel.duran@inacapmail.cl>
2025-10-27 01:59:56 -0300

    Añadiendo sistema de login con Bootstrap

--- SoftwareApp/urls.py
+++ SoftwareApp/urls.py
@@ -1,12 +1,15 @@
-
-
 from django.contrib import admin
 from django.urls import path
+from ItemApp import views as item_views
 
-from ItemApp import views as item_views 
+from django.contrib.auth import views as otras_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('', item_views.vista_inicio, name='inicio'),
+    path('login/', otras_views.LoginView.as_view(
+        template_name='login.html' 
+    ), name='login'),
+
+    path('logout/', otras_views.LogoutView.as_view(), name='logout'),
 ]
