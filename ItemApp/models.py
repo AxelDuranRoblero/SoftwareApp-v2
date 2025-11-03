334435c30304515a5c611365f9c5d644b4a9705f
Rodrigo Lara <rodrigo.lara18@inacapmail.cl>
2025-11-03 06:10:53 -0300

    dashboardpt18

--- ItemApp/models.py
+++ ItemApp/models.py
@@ -2,14 +2,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Archivo(models.Model):
     nombre = models.CharField(max_length=255)
     archivo = models.FileField(upload_to='uploads/')
     fecha_subida = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.nombre or "Archivo sin nombre")
+        return self.nombre
+
 
 
 
